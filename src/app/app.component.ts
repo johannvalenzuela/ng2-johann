550a7be1c77d73ee0dfb70a5936ac419407c3f26
Johann <johann.aevt@gmail.com>
2019-04-01 09:18:52 -0300

    feat: add Bulma librarie and extensions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import bulmaQuickview from 'bulma-quickview/dist/js/bulma-quickview';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ng2-johann';
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    const quickview = bulmaQuickview.attach();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent,
+    HeroComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
